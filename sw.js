bdd35fd3210247a7cde52709e425c954b72b8b41
yunweneric <yunweneric@mail.com>
2020-08-19 19:58:13 +0100

    initial offline fallback

--- sw.js
+++ sw.js
@@ -1,30 +1,20 @@
 const staticCacheName = 'site-static';
-const dynamicCacheName = 'site-dynamic';
+const dynamicCacheName = 'site-dynamic-v3';
 const assets = [
   '/',
   '/index.html',
-  '/js/app.js',
-  // '/js/ui.js',
-  // '/js/materialize.min.js',
-  // '/css/styles.css',
-  // '/css/materialize.min.css',
-  // '/img/dish.png',
-  // 'https://fonts.googleapis.com/icon?family=Material+Icons',
-  // 'https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2',
-  // '/pages/fallback.html',
-
-
+  '/app.js',
+  '/stylesheets/illustrations/logo.png',
+  '/stylesheets/bootstrap-4/css/bootstrap.css',
+  '/stylesheets/reset.css',
+  '/stylesheets/main.css',
+  'stylesheets/bootstrap-4/js/bootstrap.min.js',
+  '/stylesheets/jquery/jquery.min.js',
   '/script.js',
-  'stylesheets/illustrations/logo.png',
-  'stylesheets/bootstrap-4/js/bootstrap.bundle.min.js',
-  'stylesheets/bootstrap-4/css/bootstrap.css',
-  'stylesheets/reset.css',
-  'stylesheets/main.css',
   'https://fonts.googleapis.com/css2?family=Alegreya&family=B612&family=Lato&family=Muli:ital@1&display=swap',
-  'https://fonts.googleapis.com/css2?family=Caveat&display=swap',
   '/img/icons/icon-96x96.png',
-  'dictionary.json'
-
+  '/dictionary.json',
+  'https://fonts.gstatic.com/s/lato/v16/S6uyw4BMUTPHjx4wXiWtFCc.woff2'
 ];
 
 // cache size limit function
@@ -51,7 +41,7 @@ self.addEventListener('install', evt => {
 
 // activate event
 self.addEventListener('activate', evt => {
-  // //console.log('service worker activated');
+  //console.log('service worker activated');
   evt.waitUntil(
     caches.keys().then(keys => {
       //console.log(keys);
@@ -65,26 +55,30 @@ self.addEventListener('activate', evt => {
 
 // fetch events
 self.addEventListener('fetch', evt => {
-  // if (evt.request.url.indexOf('firestore.googleapis.com') === -1) {
-  //   evt.respondWith(
-  // caches.match(evt.request).then(cacheRes => {
-  //   return cacheRes || fetch(evt.request).then(fetchRes => {
-  //     return caches.open(dynamicCacheName).then(cache => {
-  //       cache.put(evt.request.url, fetchRes.clone());
-  //       // check cached items size
-  //       limitCacheSize(dynamicCacheName, 15);
-  //       return fetchRes;
-  //     })
-  //   });
-  // })
-  // .catch(() => {
-  //   if (evt.request.url.indexOf('.html') > -1) {
-  //     // return caches.match('/pages/fallback.html');
+  //   if (evt.request.url.indexOf('firestore.googleapis.com') === -1) {
+  //     evt.respondWith(
+  //       caches.match(evt.request).then(cacheRes => {
+  //         return cacheRes || fetch(evt.request).then(fetchRes => {
+  //           return caches.open(dynamicCacheName).then(cache => {
+  //             cache.put(evt.request.url, fetchRes.clone());
+  //             // check cached items size
+  //             limitCacheSize(dynamicCacheName, 15);
+  //             return fetchRes;
+  //           })
+  //         });
+  //       })
+  //         .catch(() => {
+  //           if (evt.request.url.indexOf('.html') > -1) {
+  //             return caches.match('/pages/fallback.html');
+  //           }
+  //         })
+  //     );
   //   }
-  // })
-  // );
-  // }
+  // });
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request)
+    })
+  )
 });
-
-
-// 
+//
